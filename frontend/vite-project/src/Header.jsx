c72b80cd9bd5fbbc78c9bb82f59970fafa665f92
waradis <waradis.kam@dome.tu.ac.th>
2024-04-19 13:58:02 +0700

    add react code

---
+++ frontend/vite-project/src/Header.jsx
@@ -0,0 +1,15 @@
+function Header(){
+    return(
+        <div className="app">
+            <div className="left-column">
+                test
+            </div>
+
+            <div className="right-column">
+                test
+            </div>
+        </div>
+    );
+
+}
+export default Header
