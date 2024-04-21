676f51c21024b00159b8ddda94a0379afe2dbaa9
waradis6509650088 <waradis.kam@dome.tu.ac.th>
2024-04-21 20:20:08 +0700

    frontend sidebar button

--- frontend/vite-project/src/Sidebar.jsx
+++ frontend/vite-project/src/Sidebar.jsx
@@ -1,14 +1,26 @@
+import { useState } from 'react';
 import catlogo from './res/catlogo.png'
+
+const [currentPage,setCurrentPage] = useState("main");
+
+const changeCurrentPage = (str) => {
+    setCurrentPage(str);
+}
+
 function Sidebar(){
     return(
         <div class="sidebar">
             <img src={catlogo} alt='catlogo' class='catlogo'/>
-            <br/>
             <p class="username">username</p>
             <p class="username">usertype</p>
             <br/>
             <br/>
-            <input class="sidebar-button" type="button" value="testing"/>
+            <button class="sidebar-button" onClick={() => changeCurrentPage('main')}>หน้าหลัก</button>
+            <button class="sidebar-button" onClick={() => changeCurrentPage('inventory')}>คลังสินค้า</button>
+            <button class="sidebar-button" onClick={() => changeCurrentPage('orderIn')}>รับเข้า</button>
+            <button class="sidebar-button" onClick={() => changeCurrentPage('orderlist')}>คำสั่งซื้อ</button>
+            <button class="sidebar-button" onClick={() => changeCurrentPage('customerlist')}>ลูกค้า/ผู้จัดจำหน่าย</button>
+            <text>{currentPage}</text>
         </div>
     );
 
