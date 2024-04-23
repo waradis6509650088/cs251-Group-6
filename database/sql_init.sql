db962ed75f5d8bcd8c5bc13942e4919e792e060f
waradis6509650088 <waradis.kam@dome.tu.ac.th>
2024-04-23 19:55:39 +0700

    database create table

--- database/sql_init.sql
+++ database/sql_init.sql
@@ -1,28 +1,117 @@
--- DROP TABLE IF EXISTS `credential`;
+DROP TABLE IF EXISTS Manager;
 
--- CREATE TABLE `credential` (
---   `ID` int(6) unsigned NOT NULL AUTO_INCREMENT,
---   `Name` varchar(30) NOT NULL,
---   `EID` varchar(20) DEFAULT NULL,
---   `Salary` int(9) DEFAULT NULL,
---   `birth` varchar(20) DEFAULT NULL,
---   `SSN` varchar(20) DEFAULT NULL,
---   `PhoneNumber` varchar(20) DEFAULT NULL,
---   `Address` varchar(300) DEFAULT NULL,
---   `Email` varchar(300) DEFAULT NULL,
---   `NickName` varchar(300) DEFAULT NULL,
---   `Password` varchar(300) DEFAULT NULL,
---   PRIMARY KEY (`ID`)
--- ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
--- LOCK TABLES `credential` WRITE;
+CREATE TABLE Manager (
+    ManagerID varchar(6),
+    Firstname varchar(25),
+    Lastname varchar(25),
+    District varchar(25),
+    SubDistrict varchar(25),
+    Street varchar(25),
+    Province varchar(25),
+    Zipcode int,
+    Shelf varchar(3)
+);
 
--- INSERT INTO `credential` VALUES 
--- (1,'Alice','10000',20000,'9/20','10211002','','','','','fdbe918bdae83000aa54747fc95fe0470fff4976'),
--- (2,'Boby','20000',30000,'4/20','10213352','','','','','b78ed97677c161c1c82c142906674ad15242b2d4'),
--- (3,'Ryan','30000',50000,'4/10','98993524','','','','','a3c50276cb120637cca669eb38fb9928b017e9ef'),
--- (4,'Samy','40000',90000,'1/11','32193525','','','','','995b8b8c183f349b3cab0ae7fccd39133508d2af'),
--- (5,'Ted','50000',110000,'11/3','32111111','','','','','99343bff28a7bb51cb6f22cb20a618701a2c2f58'),
--- (6,'Admin','99999','400000','3/5','43254314','','','','','a5bdf35a1df4ea895905f6f6618e83951a6effc0');
+-- DROP TABLE IF EXISTS Manager_Contact;
 
--- UNLOCK TABLES;
+-- CREATE TABLE Manager_Contact (
+--     ManagerID int,
+--     Mcontact varchar(10)
+-- );
+
+-- DROP TABLE IF EXISTS Inventory;
+
+-- CREATE TABLE Inventory (
+--     Shelf varchar(5),
+--     NumberOfProduct int(6),
+--     Leftover int(5),
+--     MaxTotalList int(5)
+-- );
+
+-- DROP TABLE IF EXISTS Supplier;
+
+-- CREATE TABLE Supplier (
+--     SupplierID varchar(6),
+--     Sname varchar(25),
+--     Contact int,
+--     List int,
+--     District varchar(25),
+--     SubDistrict varchar(25),
+--     Street varchar(25),
+--     Province varchar(25),
+--     Zipcode int,
+--     Shelf varchar(5),
+--     SendDate date,
+--     SendCount int
+-- );
+
+-- DROP TABLE IF EXISTS Retailer;
+
+-- CREATE TABLE Retailer (
+--     Rid varchar(6),
+--     Rname varchar(50),
+--     District varchar(25),
+--     SubDistrict varchar(25),
+--     Province varchar(25),
+--     Zipcode in,
+-- );
+
+-- DROP TABLE IF EXISTS Retailer_Contact;
+
+-- CREATE TABLE Retailer_Contact (
+--     Rid int,
+--     ReContact varchar(10)
+-- );
+
+-- DROP TABLE IF EXISTS Order
+
+-- CREATE TABLE Order (
+--     OrderID varchar(6),
+--     Count int,
+--     ManagerID varchar(6),
+--     Rid varchar(6)
+-- );
+
+-- DROP TABLE IF EXISTS OrderList;
+
+-- CREATE TABLE OrderList (
+--     OrderID varchar(10),
+--     List varchar(5)
+-- );
+
+-- DROP TABLE IF EXISTS Product;
+
+-- CREATE TABLE Product (
+--     Shelf varchar(5),
+--     PID int,
+--     Pname varchar(50),
+--     OrderID varchar(10),
+-- );
+
+-- DROP TABLE IF EXISTS Dryfood;
+
+-- CREATE TABLE Dryfood (
+--     Shelf varchar(5),
+--     PID varchar(6),
+--     Pname varchar(50),
+--     DryfoodFlavor varchar(50)
+-- );
+
+-- DROP TABLE IF EXISTS WetFood;
+
+-- CREATE TABLE WetFood (
+--     Shelf varchar(5),
+--     PID varchar(6),
+--     Pname varchar(50),
+--     WetFoodFlavor varchar(50)
+-- );
+
+-- DROP TABLE IF EXISTS Snacks;
+
+-- CREATE TABLE Snacks (
+--     Shelf varchar(5),
+--     PID varchar(6),
+--     Pname varchar(50),
+--     SnacksFlavor varchar(50)
+-- )
 
