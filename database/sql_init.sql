657d87f7e2b8894c9a9493863fb45cbb50d85b36
waradis6509650088 <waradis.kam@dome.tu.ac.th>
2024-04-30 20:34:37 +0700

    backend api call

--- database/sql_init.sql
+++ database/sql_init.sql
@@ -19,99 +19,99 @@ CREATE TABLE Manager_Contact (
     Mcontact varchar(10)
 );
 
--- DROP TABLE IF EXISTS Inventory;
+DROP TABLE IF EXISTS Inventory;
 
--- CREATE TABLE Inventory (
---     Shelf varchar(5),
---     NumberOfProduct int(6),
---     Leftover int(5),
---     MaxTotalList int(5)
--- );
+CREATE TABLE Inventory (
+    Shelf varchar(5),
+    NumberOfProduct int(6),
+    Leftover int(5),
+    MaxTotalList int(5)
+);
 
--- DROP TABLE IF EXISTS Supplier;
+DROP TABLE IF EXISTS Supplier;
 
--- CREATE TABLE Supplier (
---     SupplierID varchar(6),
---     Sname varchar(25),
---     Contact int,
---     List int,
---     District varchar(25),
---     SubDistrict varchar(25),
---     Street varchar(25),
---     Province varchar(25),
---     Zipcode int,
---     Shelf varchar(5),
---     SendDate date,
---     SendCount int
--- );
+CREATE TABLE Supplier (
+    SupplierID varchar(6),
+    Sname varchar(25),
+    Contact int,
+    List int,
+    District varchar(25),
+    SubDistrict varchar(25),
+    Street varchar(25),
+    Province varchar(25),
+    Zipcode int,
+    Shelf varchar(5),
+    SendDate date,
+    SendCount int
+);
 
--- DROP TABLE IF EXISTS Retailer;
+DROP TABLE IF EXISTS Retailer;
 
--- CREATE TABLE Retailer (
---     Rid varchar(6),
---     Rname varchar(50),
---     District varchar(25),
---     SubDistrict varchar(25),
---     Province varchar(25),
---     Zipcode in,
--- );
+CREATE TABLE Retailer (
+    Rid varchar(6),
+    Rname varchar(50),
+    District varchar(25),
+    SubDistrict varchar(25),
+    Province varchar(25),
+    Zipcode int
+);
 
--- DROP TABLE IF EXISTS Retailer_Contact;
+DROP TABLE IF EXISTS Retailer_Contact;
 
--- CREATE TABLE Retailer_Contact (
---     Rid int,
---     ReContact varchar(10)
--- );
+CREATE TABLE Retailer_Contact (
+    Rid int,
+    ReContact varchar(10)
+);
 
--- DROP TABLE IF EXISTS Order
+DROP TABLE IF EXISTS Ordertable;
 
--- CREATE TABLE Order (
---     OrderID varchar(6),
---     Count int,
---     ManagerID varchar(6),
---     Rid varchar(6)
--- );
+CREATE TABLE Ordertable (
+    OrderID varchar(6),
+    Count int,
+    ManagerID varchar(6),
+    Rid varchar(6)
+);
 
--- DROP TABLE IF EXISTS OrderList;
+DROP TABLE IF EXISTS OrderList;
 
--- CREATE TABLE OrderList (
---     OrderID varchar(10),
---     List varchar(5)
--- );
+CREATE TABLE OrderList (
+    OrderID varchar(10),
+    List varchar(5)
+);
 
--- DROP TABLE IF EXISTS Product;
+DROP TABLE IF EXISTS Product;
 
--- CREATE TABLE Product (
---     Shelf varchar(5),
---     PID int,
---     Pname varchar(50),
---     OrderID varchar(10),
--- );
+CREATE TABLE Product (
+    Shelf varchar(5),
+    PID int,
+    Pname varchar(50),
+    OrderID varchar(10)
+);
 
--- DROP TABLE IF EXISTS Dryfood;
+DROP TABLE IF EXISTS Dryfood;
 
--- CREATE TABLE Dryfood (
---     Shelf varchar(5),
---     PID varchar(6),
---     Pname varchar(50),
---     DryfoodFlavor varchar(50)
--- );
+CREATE TABLE Dryfood (
+    Shelf varchar(5),
+    PID varchar(6),
+    Pname varchar(50),
+    DryfoodFlavor varchar(50)
+);
 
--- DROP TABLE IF EXISTS WetFood;
+DROP TABLE IF EXISTS WetFood;
 
--- CREATE TABLE WetFood (
---     Shelf varchar(5),
---     PID varchar(6),
---     Pname varchar(50),
---     WetFoodFlavor varchar(50)
--- );
+CREATE TABLE WetFood (
+    Shelf varchar(5),
+    PID varchar(6),
+    Pname varchar(50),
+    WetFoodFlavor varchar(50)
+);
 
--- DROP TABLE IF EXISTS Snacks;
+DROP TABLE IF EXISTS Snacks;
 
--- CREATE TABLE Snacks (
---     Shelf varchar(5),
---     PID varchar(6),
---     Pname varchar(50),
---     SnacksFlavor varchar(50)
--- )
+CREATE TABLE Snacks (
+    Shelf varchar(5),
+    PID varchar(6),
+    Pname varchar(50),
+    SnacksFlavor varchar(50)
+)
 
